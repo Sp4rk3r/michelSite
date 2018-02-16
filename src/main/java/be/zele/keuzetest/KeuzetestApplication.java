d34d64296a67357fa77715b35b0e8ba4a5cd33aa
Sp4rk3r <koenrombout@hotmail.com>
2018-02-16 17:19:38 +0100

    Add files via upload

---
+++ src/main/java/be/zele/keuzetest/KeuzetestApplication.java
@@ -0,0 +1,12 @@
+package be.zele.keuzetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeuzetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeuzetestApplication.class, args);
+	}
+}
---
+++ src/test/java/be/zele/keuzetest/KeuzetestApplicationTests.java
@@ -0,0 +1,16 @@
+package be.zele.keuzetest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeuzetestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
